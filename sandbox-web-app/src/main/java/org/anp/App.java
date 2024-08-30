c7f06f4ea14a11b0179e0cc6fb748ad6e32ff779
anooptiwari2402 <anoop.ece.nitap@gmail.com>
2024-08-31 01:10:18 +0530

    Added some end points

--- sandbox-web-app/src/main/java/org/anp/App.java
+++ sandbox-web-app/src/main/java/org/anp/App.java
@@ -2,6 +2,14 @@ package org.anp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import java.time.Instant;
+
+import java.util.TimerTask;
 
 @SpringBootApplication
 public class App {
@@ -9,3 +17,18 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 }
+
+@RestController
+@RequestMapping("/app")
+class AppController{
+    @GetMapping("/health")
+    public Mono<ResponseEntity<String>> healthCheck(){
+        return Mono.just(ResponseEntity.ok("Working"));
+    }
+    @GetMapping("/instant")
+    public Mono<ResponseEntity<Instant>> instant(){
+        return Mono.just(ResponseEntity.ok(Instant.now()));
+    }
+}
+
+
